c48d8dfaa1145f9043e9aa3a89ad51051a550a52
lee gunhee <rlaclejr@gmail.com>
2022-05-11 12:06:06 +0900

    메인페이지 확인용

---
+++ src/main/java/com/homefix/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.homefix.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping(name = "/")
+	public String mainPage() {
+		
+		return "index";
+	}
+}
