a5cc4aee3385d66ce636e97f396aea99a2af6bdd
Bumsik123 <100894641+Bumsik123@users.noreply.github.com>
2022-05-11 12:16:40 +0900

    페이지보기

--- src/main/java/com/homefix/controller/MainController.java
+++ src/main/java/com/homefix/controller/MainController.java
@@ -2,6 +2,8 @@ package com.homefix.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
@@ -10,5 +12,10 @@ public class MainController {
 	public String mainPage() {
 		
 		return "index";
+	}
+	
+	@GetMapping("/{step}")
+	public void viewPage(@PathVariable String step) {
+		//return "/board/" + step;
 	}
 }
