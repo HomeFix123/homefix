46a4966c3c7ab33191bb94823bc7bf1b0be2e5e2
lee gunhee <rlaclejr@gmail.com>
2022-05-11 10:02:46 +0900

    기본틀

---
+++ src/main/java/com/homefix/HomefixApplication.java
@@ -0,0 +1,13 @@
+package com.homefix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomefixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomefixApplication.class, args);
+	}
+
+}
