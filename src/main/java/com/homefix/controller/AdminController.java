655ef35077ba3c286c745a9434b5db8e3ed3621c
chocopaisa <rlaclejr@gmail.com>
2022-05-13 21:42:44 +0900

    고객 페이지 화면 구현

--- src/main/java/com/homefix/controller/AdminController.java
+++ src/main/java/com/homefix/controller/AdminController.java
@@ -2,12 +2,17 @@ package com.homefix.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
+	@GetMapping(path = "/")
+	public String moveAdminPage() {
+		
+		return "/admin/member";
+	}
 	
 	@GetMapping(path = "/dashboard")
 	public String moveDashBoardPage() {
