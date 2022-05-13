b063aec45ddea052032cfffe38403a04d323ef7a
chocopaisa <rlaclejr@gmail.com>
2022-05-13 12:35:12 +0900

    메인페이지 이동 및 수정

---
+++ src/main/java/com/homefix/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.homefix.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@GetMapping(path = "/dashboard")
+	public String moveAdminPage() {
+		System.out.println("s");
+		return "/admin/dashboard";
+	}
+}
