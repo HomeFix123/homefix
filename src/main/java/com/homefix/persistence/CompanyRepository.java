c9f9207d1dad35f342ed805b60330ad589b1e304
eunhyeLee <wpaa@naver.com>
2022-05-20 17:25:33 +0900

    사업자 crud
    사업자 crud, 결제페이지

--- src/main/java/com/homefix/persistence/CompanyRepository.java
+++ src/main/java/com/homefix/persistence/CompanyRepository.java
@@ -1,9 +1,19 @@
 package com.homefix.persistence;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
 import com.homefix.domain.Company;
+
 
 public interface CompanyRepository extends CrudRepository<Company, String>{
 
+	public Long countById(String id);
+	
+	public List<Company> findByEmail(String email) ;
+	
+	public Long countByNum(String num);
+	
+	
+	
 }
