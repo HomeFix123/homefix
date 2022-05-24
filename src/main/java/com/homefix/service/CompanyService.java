c8755bfb4aea78a3c0093a49e7d94caf7b98e659
eunhyeLee <wpaa@naver.com>
2022-05-24 18:13:24 +0900

    사업자 회원정보 불러오기
    사업자 회원정보 마이페이지에 불러오기

--- src/main/java/com/homefix/service/CompanyService.java
+++ src/main/java/com/homefix/service/CompanyService.java
@@ -14,7 +14,6 @@ public interface CompanyService {
 
 
 	// 사업자 아이디 중복 조회
-	/* public Company idCheck(String id); */
 	public String  idCheck(String id); 
 	
 	
@@ -31,6 +30,10 @@ public interface CompanyService {
 	// 사업자 회원 탈퇴
 	public void companyDelete(String id);
 
+	//사업자 정보 조회
+	public Company getCompanyMyInfo(String companyId);
+	
+	
 	// 사업자 정보수정
 	public void companyUpdate(CompanyDto dto);
 
