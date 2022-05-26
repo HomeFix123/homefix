11dd05cab00784980f70213b7b805ee5158ff267
eunhyeLee <wpaa@naver.com>
2022-05-26 17:44:46 +0900

    사업자정보수정후 저장
    로고수정은 아직 안함

--- src/main/java/com/homefix/domain/CompanyDto.java
+++ src/main/java/com/homefix/domain/CompanyDto.java
@@ -1,5 +1,9 @@
 package com.homefix.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -12,37 +16,43 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
+//@Table(name = "company")
 public class CompanyDto {
 
+	@Id
+	@Column(name="cid")
 	private String id;
-	
+	@Column(name="co_pass")
 	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
 	@Pattern(regexp = "/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\\\(\\\\)\\-_=+]).{8,16}$/", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
 	private String pass;
 	
 	private String passCheck;
-	
+	@Column(name="co_name")
 	@Pattern(regexp = "/^[가-힣a-zA-Z0-9]{1,10}$/", message = "한글, 영문 그리고 숫자만 입력 가능합니다.")
 	private String name;
-	
+	@Column(name="co_ceo")	
 	@Pattern(regexp = "/^[가-힣]+$/",message="한글만 입력 가능합니다.")
 	private String ceo;
-	
+	@Column(name="co_num",unique=true)
 	private String num;
-	
+	@Column(name="co_tel")
 	@Pattern(regexp = "/^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}/",message="전화번호 형식이 올바르지 않습니다.")
 	private String tel;
-	
+	@Column(name="co_zip")
 	@NotBlank(message = "우편번호는 필수 입력 값입니다.")
 	private String zip;
+	@Column(name="co_addr")
 	@NotBlank(message = "주소는 필수 입력 값입니다.")
 	private String addr;
+	@Column(name="co_addr_d")
 	@NotBlank(message = "상세주소는 필수 입력 값입니다.")
 	private String addrd;
 	
 	
 	
-	
+	@Column(name="co_email")	
 	@NotBlank(message = "이메일은 필수 입력 값입니다.")
 	@Pattern(regexp = "/^([\\w-]+(?:\\.[\\w-]+)*)@((?:[\\w-]+\\.)*\\w[\\w-]{0,66})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$/i", message = "이메일 형식이 올바르지 않습니다.")
 	private String email;	
--- src/main/java/com/homefix/service/CompanyServiceImpl.java
+++ src/main/java/com/homefix/service/CompanyServiceImpl.java
@@ -1,17 +1,11 @@
 package com.homefix.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
 
 import com.homefix.domain.Company;
-import com.homefix.domain.CompanyDto;
 import com.homefix.persistence.CompanyRepository;
 
 @Service
@@ -61,36 +55,47 @@ public class CompanyServiceImpl implements CompanyService {
 	public void companyDelete(String id) {
 		companyRepo.deleteById(id);
 	}
-	
-	
-	//사업자 정보 불러오기
+
+	// 사업자 정보 불러오기
 	public Company getCompanyMyInfo(String companyId) {
 		return companyRepo.findById(companyId).get();
 	}
-	
-	
-	
+
 	// 사업자 정보수정
-	public void companyUpdate(CompanyDto dto) {
-		companyRepo.save(dto);
+	public void companyUpdate(Company com) {
+		Company company = companyRepo.findById(com.getId()).get();
+		
+		company.setAddr(com.getAddr()); 
+		company.setAddrd(com.getAddrd());
+		company.setEmail(com.getEmail());
+		company.setName(com.getName());
+		company.setTel(com.getTel());
+		company.setPass(com.getPass());
+		company.setZip(com.getZip());
+		
+		
+		
+		companyRepo.save(company);
 	}
 
 	// 사업자 회원가입
 	public void companyInsert(Company com) {
+		
+		
 		companyRepo.save(com);
 	}
-
-	/* 회원가입 시, 유효성 체크 */
-	@Transactional(readOnly = true)
-	public Map<String, String> validateHandling(Errors errors) {
-
-		Map<String, String> validatorResult = new HashMap<>();
-		/* 유효성 검사에 실패한 필드 목록을 받음 */
-		for (FieldError error : errors.getFieldErrors()) {
-			String validKeyName = String.format("valid_%s", error.getField());
-			validatorResult.put(validKeyName, error.getDefaultMessage());
-		}
-		return validatorResult;
-	}
+  
+	/*
+	 * 회원가입 시, 유효성 체크
+	 * 
+	 * @Transactional(readOnly = true) public Map<String, String>
+	 * validateHandling(Errors errors) {
+	 * 
+	 * Map<String, String> validatorResult = new HashMap<>(); 유효성 검사에 실패한 필드 목록을 받음
+	 * for (FieldError error : errors.getFieldErrors()) { String validKeyName =
+	 * String.format("valid_%s", error.getField());
+	 * validatorResult.put(validKeyName, error.getDefaultMessage()); } return
+	 * validatorResult; }
+	 */
 
 }
