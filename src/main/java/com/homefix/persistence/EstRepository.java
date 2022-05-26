c228e0015470b53cfec4d03066a6888baec2b1c9
hi0eun <20eunhi@gmail.com>
2022-05-26 14:53:25 +0900

    업체견적
    업체한테 직접온 견적 업체 아이디 별로 뜨게 하는거랑 로그인페이지는 테스트하는데 필요한 페이지입니다.

--- src/main/java/com/homefix/persistence/EstRepository.java
+++ src/main/java/com/homefix/persistence/EstRepository.java
@@ -1,5 +1,8 @@
 package com.homefix.persistence;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.homefix.domain.Estimation;
@@ -8,5 +11,6 @@ import com.homefix.domain.Estimation;
 CRUD 기능을 처리하는 인터페이스로 기존의 DAO 역할로 DB 처리 */     //엔티티 클래스, @Id 매핑되는 변수의 타입
 public interface EstRepository extends CrudRepository<Estimation, Integer>{
 
-
+	@Query(value="SELECT building,size,budget,eaddr,ename FROM estimation WHERE cid = ?1" , nativeQuery = true)
+	List<Object[]> queryAnnotation(String word);
 }
