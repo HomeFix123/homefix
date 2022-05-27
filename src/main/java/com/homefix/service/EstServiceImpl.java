d910e534fcd5781e12173ff89bc7db878d1df4dd
hi0eun <20eunhi@gmail.com>
2022-05-27 09:53:08 +0900

    JPA
    MPickC요청 JPA로 변경했습니다.

--- src/main/java/com/homefix/service/EstServiceImpl.java
+++ src/main/java/com/homefix/service/EstServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.homefix.domain.Company;
 import com.homefix.domain.Estimation;
 import com.homefix.persistence.CompanyRepository;
 import com.homefix.persistence.EstRepository;
@@ -17,6 +18,21 @@ public class EstServiceImpl implements EstService {
 	
 	@Autowired
 	CompanyRepository companyRepo;
+
+	@Override
+	public Estimation getEstDetail(String id) {
+		Estimation resultData = estRepo.getEstDetail(id);
+		System.out.println("resultData는"+resultData);
+		return resultData;
+	}
+
+	@Override
+	public List<Estimation> getCEst(String cid) {
+		Company com = companyRepo.findById(cid).get();
+		return estRepo.findByCompany(com);
+	}
+	
+	
 
 	
 	
