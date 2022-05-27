982430395829f1713254b06fe34c47283181788b
kjy5984 <kjy5984@hanmail.net>
2022-05-27 10:04:25 +0900

    견적신청 페이지 수정
    견적신청 페이지 수정

--- src/main/java/com/homefix/persistence/EstRepository.java
+++ src/main/java/com/homefix/persistence/EstRepository.java
@@ -7,6 +7,7 @@ import com.homefix.domain.Estimation;
 /* JPA Repository 란?
 CRUD 기능을 처리하는 인터페이스로 기존의 DAO 역할로 DB 처리 */     //엔티티 클래스, @Id 매핑되는 변수의 타입
 public interface EstRepository extends CrudRepository<Estimation, Integer>{
-
+	
+	
 
 }
