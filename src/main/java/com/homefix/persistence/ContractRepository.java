96b615a3232e6f302ac5ed85e489f11139aa9f15
hi0eun <20eunhi@gmail.com>
2022-05-31 17:39:22 +0900

    CIng완성

--- src/main/java/com/homefix/persistence/ContractRepository.java
+++ src/main/java/com/homefix/persistence/ContractRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.homefix.domain.Company;
 import com.homefix.domain.Contract;
+import com.homefix.domain.Estimation;
 
 
 public interface ContractRepository extends CrudRepository<Contract, Integer>{
 		List<Contract> findByCompay(Company compay);
+		
+		Contract findByEstimation(Estimation estimation);
 }
