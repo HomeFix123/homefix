0bec0f7ebf69d9bda42509eef580ad5576f8e8f1
eunhyeLee <wpaa@naver.com>
2022-06-02 18:32:01 +0900

    결제 완료, 탈퇴 기능 구현중
    결제 DB연결 완료,
    탈퇴 기능 구현중

--- src/main/java/com/homefix/domain/Payment.java
+++ src/main/java/com/homefix/domain/Payment.java
@@ -5,11 +5,14 @@ import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
@@ -19,14 +22,23 @@ import lombok.Data;
 public class Payment {
 	@Id
 	private String pid;
+
 	
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy. MM. dd.")
 	@Column(name = "pm_day")
 	private Date pday;
 	private Integer amount;
 	@Column(name = "pm_method")
 	private String method;
-	
+
 	@JoinColumn(name = "cid")
-	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
 	private Company company;
+
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy. MM. dd.")
+	@Column(name = "pm_last")
+	private Date plast;
+
 }
