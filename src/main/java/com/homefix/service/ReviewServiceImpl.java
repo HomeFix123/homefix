0ec7d683ddf053f6a25b9ea12ccca55021c7f5fb
Bumsik123 <100894641+Bumsik123@users.noreply.github.com>
2022-06-03 19:25:02 +0900

    자랑 작성 업체, 시공 후기 기반
    테스트 안해봄

--- src/main/java/com/homefix/service/ReviewServiceImpl.java
+++ src/main/java/com/homefix/service/ReviewServiceImpl.java
@@ -6,8 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.homefix.domain.Brag;
+import com.homefix.domain.Company;
+import com.homefix.domain.CompanyReport;
+import com.homefix.domain.Member;
 import com.homefix.domain.Review;
+import com.homefix.persistence.CompanyReportRepository;
 import com.homefix.persistence.CompanyRepository;
 import com.homefix.persistence.MemberRepository;
 import com.homefix.persistence.ReviewRepository;
@@ -18,12 +21,19 @@ public class ReviewServiceImpl implements ReviewService {
 	@Autowired
 	ReviewRepository reviewRepo;
 	
-	/*
-	 * @Autowired MemberRepository memberRepo;
-	 */
+	
+	@Autowired 
+	MemberRepository memberRepo;
+	 
 	
 	@Autowired
 	CompanyRepository companyRepo;
+	
+	@Autowired
+	CompanyReportRepository companyRRepo;
+	
+	
+	
 	
 	
 	@Override
@@ -43,6 +53,42 @@ public class ReviewServiceImpl implements ReviewService {
 		
 		return (List<Review>)reviewRepo.findAll();
 	}
+	
+	@Override
+	public Review getReview(Review rev ) {
+		
+		return reviewRepo.findByRid(rev.getRid());
+		
+	}
+	
+	@Override
+	public void deleteReview(Review rev, String cid) {
+		Review result = reviewRepo.findByRid(rev.getRid());
+		result.setCompany(companyRepo.findById(cid).get());
+		reviewRepo.delete(reviewRepo.findByRidAndCompany(result.getRid(), result.getCompany()));
+	}
+	
+	
+	@Override
+	public String saveCReport(Company cid, String id, String reason) {
+		Member repo = memberRepo.findById(id).get();
+		List<CompanyReport> list = companyRRepo.findByCompanyAndMember(id, repo);
+		if(list.isEmpty()) {
+			CompanyReport result = new CompanyReport();
+			result.setCompany(cid);
+			result.setMember(repo);
+			result.setReason(reason);
+			companyRRepo.save(result);
+			return "true";
+			
+		} else {
+			
+			return "false";
+		
+		}
+	}
+	
+	
 	
 	
 }
