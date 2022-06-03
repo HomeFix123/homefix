5cf6db4e337e3b1f8a2c0cc698e443ec7ac1c6ca
Bumsik123 <100894641+Bumsik123@users.noreply.github.com>
2022-06-03 16:04:44 +0900

    자랑 신고 구현, 삭제 Cascade 해야함
    자랑 작성 계약한 업체리스트

--- src/main/java/com/homefix/persistence/ContractRepository.java
+++ src/main/java/com/homefix/persistence/ContractRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.homefix.domain.Company;
 import com.homefix.domain.Contract;
+import com.homefix.domain.Estimation;
 
 
 public interface ContractRepository extends CrudRepository<Contract, Integer>{
+
+	Contract findByEstimation(Estimation estimation);
+
+	
 		
 }
