b94ae9e3ed29f0aa26b6d9c6fb1645b3b5d538eb
eunhyeLee <wpaa@naver.com>
2022-06-05 17:55:07 +0900

    사업자 마이페이지, 업체 상세페이지
    crud 아직 미완성

--- src/main/java/com/homefix/persistence/ReviewRepository.java
+++ src/main/java/com/homefix/persistence/ReviewRepository.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import com.homefix.domain.Company;
 import com.homefix.domain.Review;
 
 public interface ReviewRepository extends CrudRepository<Review, String>{
 	
 	public List<Review> findAll(Pageable pageable);
+	
+	public Review findByCompany(Company com);
 }
