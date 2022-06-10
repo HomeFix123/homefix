88b8dbcaf0de374b9d6c77e0d63f0562d42f580f
hi0eun <20eunhi@gmail.com>
2022-06-10 19:04:40 +0900

    font 적용
    주석처리/font적용

--- src/main/java/com/homefix/persistence/Esti_requestRepository.java
+++ src/main/java/com/homefix/persistence/Esti_requestRepository.java
@@ -12,7 +12,7 @@ import com.homefix.domain.Estimation;
 public interface Esti_requestRepository extends CrudRepository<Esti_request, Integer>{
 	//견적 아이디로 커넥트 리스트 찾기
 	public List<Esti_request> findByEstimation(Estimation estimation);
-	
+	//견적과 회사로 견적 요청 찾기
 	public Esti_request findByEstimationAndCompany(Estimation estimation,Company company);
 	
 }
